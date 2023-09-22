d63098fc03d675c02f0c5461a31686c26b5d1dd2
vaishnaviatmobiusodotcom <93509474+vaishnaviatmobiusodotcom@users.noreply.github.com>
2023-09-22 21:54:42 +0530

    connecting to postgres database using sequelize

---
+++ src/core/database/database.provider.ts
@@ -0,0 +1,69 @@
+import { Sequelize } from 'sequelize-typescript';
+import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../constants';
+import { databaseConfig } from './db.config';
+import { User } from 'src/modules/users/user.entity';
+import { Client } from 'pg';
+import * as pg from 'pg';
+
+export const databaseProviders = [
+  {
+    provide: SEQUELIZE,
+    useFactory: async () => {
+      let config;
+      switch (process.env.NODE_ENV) {
+        case DEVELOPMENT:
+          config = databaseConfig.development;
+          break;
+        case TEST:
+          config = databaseConfig.test;
+          break;
+        case PRODUCTION:
+          config = databaseConfig.production;
+          break;
+        default:
+          config = databaseConfig.development;
+      }
+      try {
+        const { host, port, username, password, database } = config;
+        const client = new Client({
+          user: username,
+          password: password,
+          host: host,
+        });
+
+        client.connect();
+        let res =  await client.query(`SELECT 1 from pg_database WHERE datname = '${database}'`);
+        if (
+         res.rowCount == 1
+          // `$( psql -XtAc "SELECT 1 FROM pg_database WHERE datname='${database}'" )" = '1`
+          // `psql ${database} -c '\q' 2>&1`
+        ) {
+
+          console.log('Database already exists!');
+          const sequelize = new Sequelize(config);
+          sequelize.addModels([User]);
+          await sequelize.sync();
+          console.log('Connection has been established successfully.');
+          return sequelize;
+         
+        } else {
+          console.log("Database does not exist!");
+         
+          client.query(`CREATE DATABASE ${database};`, async (err, res) => {
+            console.log('err', err);
+            console.log('res', res);
+
+            const sequelize = new Sequelize(config);
+            sequelize.addModels([User]);
+            await sequelize.sync();
+            console.log('Connection has been established successfully.');
+            client.end();
+            return sequelize;
+          });
+        }
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+    },
+  },
+];
---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users..module';
+import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule,
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWTKEY,
+      signOptions: { expiresIn: process.env.TOKEN_EXPIRATION },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
